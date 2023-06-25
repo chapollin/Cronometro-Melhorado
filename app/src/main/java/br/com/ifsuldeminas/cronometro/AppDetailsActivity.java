e3a1ee466aafb9dbe95546662263e5ea3ec39667
m <m@gmail.com>
2023-06-25 16:57:29 -0300

    atualizadasso

--- app/src/main/java/br/com/ifsuldeminas/cronometro/AppDetailsActivity.java
+++ app/src/main/java/br/com/ifsuldeminas/cronometro/AppDetailsActivity.java
@@ -14,20 +14,21 @@ public class AppDetailsActivity extends AppCompatActivity {
 
         TextView tvAppDetails = findViewById(R.id.tvAppDetails);
         String appDetails = "Cronômetro App\n\n" +
-                "Versão: 1.0\n\n" +
+                "Versão: 1.2\n\n" +
                 "Descrição:\n" +
-                "O aplicativo de cronômetro permite que você meça o tempo com precisão. " +
+                "O aplicativo de cronômetro permite que você meça o tempo com precisão de Millisegundos. " +
                 "Você pode iniciar, pausar, reiniciar o cronômetro e salvar os tempos medidos. " +
                 "Além disso, você pode configurar se deseja ou não exibir os milissegundos no cronômetro.\n\n" +
                 "Instruções de uso:\n" +
                 "- Toque no botão 'Iniciar' para iniciar o cronômetro.\n" +
-                "- Toque no botão 'Pausar' para pausar o cronômetro. Toque novamente para continuar a contagem.\n" +
+                "- Toque no botão 'Pausar' para pausar o cronômetro. Toque em continuar para dar continuidade a contagem.\n" +
                 "- Toque no botão 'Reiniciar' para zerar o cronômetro.\n" +
                 "- Toque no botão 'Salvar' para salvar o tempo medido. Os tempos salvos podem ser acessados na tela 'Tempos Salvos'.\n" +
                 "- Toque no botão 'Tempos Salvos' para visualizar a lista de tempos salvos.\n" +
-                "- Toque no botão 'Settings' para acessar as configurações do aplicativo.\n\n" +
-                "Desenvolvido por: Seu Nome\n" +
-                "Contato: seuemail@example.com";
+                "- Toque no botão '⁝' para visualizar a lista de telas.\n" +
+                "- Toque no botão 'Detalhes do Aplicativo' para visualizar as informações do APP.\n" +
+                "- Toque no botão 'Configurações' para acessar a configuração do aplicativo.\n\n" +
+                "Desenvolvido por: Marcos Daniel de Souza Cardoso";
 
         tvAppDetails.setText(appDetails);
     }
